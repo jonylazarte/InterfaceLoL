01744aeb4c60f2f9a932dcbed9157a825cb3c8af
jonylazarte <85182911+jonylazarte@users.noreply.github.com>
2025-11-29 22:06:00 -0300

    Update bag.jsx

--- src/pages/Bag/bag.jsx
+++ src/pages/Bag/bag.jsx
@@ -1,12 +1,10 @@
 import Pokedex from '@/pages/Pokedex/index.jsx'
-import Pokemon from '@/pages/Pokemon/pokemon.jsx'
 import './bag.css'
 import {useState, memo} from 'react'
 
 
 export default memo(function Bag(){
 	const [actualSection, setActualSection] = useState("Pokemon")
-	const buttonPokemon = actualSection == "Pokemon" ? {borderBottom:"2px solid #CDBE91 ", color:"#F0E6D2"} : null
     const buttonPokedex = actualSection == "Pokedex" ? {borderBottom:"2px solid #CDBE91 ", color:"#F0E6D2"} : null
 
     const handleSound = (sound) => {
@@ -16,10 +14,8 @@ export default memo(function Bag(){
 
 	return <section className="bag">
 	<header className="bag-header">
-	<div style={buttonPokemon} className="subheader-item" onClick={()=>{handleSound('menu-click'); setActualSection("Pokemon")}}>POKEMON</div>
 	<div style={buttonPokedex} className="subheader-item" onClick={()=>{handleSound('menu-click'); setActualSection("Pokedex")}}>POKEDEX</div></header>
 
-		{actualSection == "Pokemon" && <Pokemon></Pokemon>}
 		{actualSection == "Pokedex" && <Pokedex></Pokedex>}
 
 	</section>
